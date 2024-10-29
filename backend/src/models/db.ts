0f91baafd4138b11707f1880698a695d42308961
siddharth goyal <113788213+siddharthgoyal00@users.noreply.github.com>
2024-10-29 18:58:31 +0530

    admin database schema

---
+++ backend/src/models/db.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+mongoose.connect("mongodb+srv://admin:fvmQ1D2B6iDKmUM6@cluster0.ztkcnap.mongodb.net/SofteousAlign");
+const adminSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  }, 
+  password: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", adminSchema);
+export default User;
