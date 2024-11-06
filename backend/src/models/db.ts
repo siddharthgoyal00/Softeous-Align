16f5e75c1e8b8d664d036e5b1a958fdfd8116aac
siddharth goyal <113788213+siddharthgoyal00@users.noreply.github.com>
2024-11-06 18:49:23 +0530

    type errors solved

--- backend/src/models/db.ts
+++ backend/src/models/db.ts
@@ -1,6 +1,7 @@
-import mongoose from "mongoose";
+import mongoose  from "mongoose";
 mongoose.connect("mongodb+srv://admin:fvmQ1D2B6iDKmUM6@cluster0.ztkcnap.mongodb.net/SofteousAlign");
-const adminSchema = new mongoose.Schema({
+
+const adminSchema = new mongoose.Schema ({
   companyname : {
     type: String,
     required: true,
@@ -23,6 +24,7 @@ const adminSchema = new mongoose.Schema({
     required: true,
     trim: true
   },
+})
   // createdAt: {
   //   type: Date,
   //   default: Date.now,
@@ -31,7 +33,58 @@ const adminSchema = new mongoose.Schema({
   //   type: Date,
   //   default: Date.now,
   // },
+  const employeeSchema = new mongoose.Schema({
+    
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    }, 
+    password: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    phoneNo: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    address: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    jobTitle: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    salary: {
+      type: String,
+      required: true,
+      trim: true,
+    },
 });
 
 const Admin = mongoose.model("Admin", adminSchema);
-export default Admin;
+const Employee = mongoose.model("Employee", employeeSchema)
+export  {Admin, Employee};
+
