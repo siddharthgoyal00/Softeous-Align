220b0d7df6c7543b3396bd7fb7b0443ca7ccebe1
siddharth goyal <113788213+siddharthgoyal00@users.noreply.github.com>
2024-11-06 19:02:06 +0530

    navbar changed

--- frontend/softeous-align/src/components/Navbar.tsx
+++ frontend/softeous-align/src/components/Navbar.tsx
@@ -1,7 +1,18 @@
-export const Navbar = ()=>{
-    return (
-        <div className="bg-white flex drop-shadow items-center justify-between px-6 py-4">
-        <h3 className="text-2xl font-medium">Softeous Align</h3>
-      </div>
-    )
-}
+import { FaBars } from "react-icons/fa";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <nav className="fixed top-0 left-0 w-full bg-gray-700 p-5 flex  justify-between">
+        <div className="flex items-center">
+          <button className="text-white">
+            <FaBars />
+          </button>
+          <h1 className="ml-4 text-white text-2xl font-semibold">
+            Office Align
+          </h1>
+        </div>
+      </nav>
+    </div>
+  );
+};
