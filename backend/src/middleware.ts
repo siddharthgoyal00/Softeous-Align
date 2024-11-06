de04ce883efa4ff58acb54ec335e26b63c72e673
siddharth goyal <113788213+siddharthgoyal00@users.noreply.github.com>
2024-11-06 18:48:45 +0530

    decodes the token

--- backend/src/middleware.ts
+++ backend/src/middleware.ts
@@ -2,24 +2,23 @@ import { JWT_SECRET } from "./config";
 import jwt, { JwtPayload } from "jsonwebtoken";
 const {verify} = jwt;
 export const authMiddleware = (req: any, res: any, next:any) => {     
+    try {
+        console.log(req.headers)
     const authHeader = req.headers['authorization'];    
     console.log("Authorization Header:", authHeader); // Debug
-    if (!authHeader || !authHeader.startsWith('Bearer ')) { 
+    if (!authHeader || !authHeader.startsWith('Bearer')) { 
         console.log("No valid Authorization header found"); 
-         res.status(403).json({});  
+        return  res.status(403).json({});  
     }
 
     const token = authHeader.split(' ')[1];
 
-    try {
-        const decoded = verify(token, JWT_SECRET) as JwtPayload;
+   
+        const decoded:any = verify(token, JWT_SECRET) as JwtPayload;
         console.log("Decoded JWT Payload:", decoded); // Debugging
-        if(req.body.adminId!=null){
-            req.body.adminId = decoded.adminId;
-        }else{
-            throw new Error("admin id not found!")
-        }
-          
+      if(!decoded && (decoded.adminId)) {
+        throw new Error("Admin not found or invalid token")
+      }    
         next();
     } catch (err) {
         console.error("JWT Verification Failed:", err);
