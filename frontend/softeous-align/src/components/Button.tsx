7b9f2795a39533a2997559e5f472afa6e824e5a1
siddharth goyal <113788213+siddharthgoyal00@users.noreply.github.com>
2024-11-06 18:51:13 +0530

    button color changed

--- frontend/softeous-align/src/components/Button.tsx
+++ frontend/softeous-align/src/components/Button.tsx
@@ -4,5 +4,5 @@ interface B_type{
     }
     export function Button(this: any, {label, onClick}: B_type) {
         return <button onClick={onClick.bind(this,"12"
-        )} type="button" className="w-full text-white bg-blue-700 hover:bg-gblue-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-base px-5 py-2.5 me-2 mb-2 text-lg">{label}</button>
+        )} type="button" className="w-full text-white bg-gray-700 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-base px-5 py-2.5 me-2 mb-2 text-lg">{label}</button>
     }
