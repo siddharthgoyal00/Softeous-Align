83b1291c4239e77a8d92618209687303120b1c14
siddharth goyal <113788213+siddharthgoyal00@users.noreply.github.com>
2024-11-06 19:03:26 +0530

    employee route added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express  from "express"
 import cors  from "cors"
 import dotenv from 'dotenv';
 import { adminRouter } from "./routes/admin";
+import { employeeRouter } from "./routes/employee";
 dotenv.config();  // This will load variables from .env into process.env
 
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use( "/admin" ,adminRouter ); 
+app.use("/employee" , employeeRouter)
 
  app.listen(process.env.PORT , ()=>{
     console.log(`server is running on the port ${process.env.PORT}`)
