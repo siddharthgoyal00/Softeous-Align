e1590b50ec48bfddba0e04fd7bad9fe41e842579
siddharth goyal <113788213+siddharthgoyal00@users.noreply.github.com>
2024-11-07 20:21:39 +0530

    cards merge to the heSection component

--- frontend/softeous-align/src/pages/Home/Home.tsx
+++ frontend/softeous-align/src/pages/Home/Home.tsx
@@ -1,13 +1,13 @@
-import { Cards } from "../../components/Cards";
+
 import { HeroSection } from "../../components/HeroSection";
-import { Navbar } from "../../components/Navbar";
+
 
 export const Home = () => {
   return (
     <div className="min-h-screen flex flex-col">
     
      <HeroSection></HeroSection>
-     <Cards></Cards>
+     
   </div>
   );
 };
